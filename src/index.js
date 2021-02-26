8d55c3b158707362341faf938f86cf99ee51fd86
Gisela Donoso <giselapazvivian@gmail.com>
2021-02-26 11:24:04 -0300

    proyecto final

--- src/index.js
+++ src/index.js
@@ -1,88 +1,62 @@
 import cipher from './cipher.js';
 //Declarar variables
-const botonCambioPagina=document.getElementById("botonCambioPagina");
-const botonVolver=document.getElementById("botonVolver");
-
+const botonCambioPagina = document.getElementById("botonCambioPagina");
+const botonVolver = document.getElementById("botonVolver");
+const cifrar = document.getElementById("botonCifrar");
+const descifrar = document.getElementById("botonDescifrar");
 
 //ir de página 1 a 2
-
-const cambioPagina= () => {
+const cambioPagina = () => {
     document.querySelector(".primeraPagina").style.display = "none";
-   document.querySelector(".segundaPagina").style.display ="block";
+    document.querySelector(".segundaPagina").style.display = "block";
 }
 botonCambioPagina.addEventListener("click", cambioPagina);
 
-
-
 //Cambiar letras minúsculas por mayúsculas para cifrar
-const mayuscula= () => {
-  let x = document.getElementById("mensajeCifrar");
-  x.value = x.value.toUpperCase();
+const mayuscula = () => {
+    let x = document.getElementById("mensajeCifrar");
+    x.value = x.value.toUpperCase();
 }
 document.getElementById("mensajeCifrar").addEventListener("keyup", mayuscula);
 
-
-
-
 //Botón Cifrar
-
-const cifrar=document.getElementById("botonCifrar")
-const botonCifrar= () => {
+const botonCifrar = () => {
     let numeroCifrar = document.getElementById("numeroCifrar").value;
     let mensajeCifrar = document.getElementById("mensajeCifrar").value;
     let mensajeCifrado = cipher.encode(numeroCifrar, mensajeCifrar);
-    document.getElementById("mensajeCifrar").value=mensajeCifrado;
-    
-    console.log(numeroCifrar);
-    console.log(mensajeCifrar);
-    console.log(mensajeCifrado);
+    document.getElementById("mensajeCifrar").value = mensajeCifrado;
+
+    // console.log(numeroCifrar);
+    // console.log(mensajeCifrar);
+    // console.log(mensajeCifrado);
 }
 cifrar.addEventListener("click", botonCifrar);
 
-
-
 //Cambiar letras minúsculas por mayúsculas para descifrar
-const mayuscula1= () => {
+const mayuscula1 = () => {
     let x = document.getElementById("mensajeDescifrar");
     x.value = x.value.toUpperCase();
-  }
-  document.getElementById("mensajeDescifrar").addEventListener("keyup", mayuscula1);
+}
+document.getElementById("mensajeDescifrar").addEventListener("keyup", mayuscula1);
 
 //Botón Descifrar
-
-const descifrar=document.getElementById("botonDescifrar")
-const botonDescifrar= () => {
+const botonDescifrar = () => {
     let numeroDescifrar = document.getElementById("numeroDescifrar").value;
     let mensajeDescifrar = document.getElementById("mensajeDescifrar").value;
     let mensajeDescifrado = cipher.decode(numeroDescifrar, mensajeDescifrar);
-    document.getElementById("mensajeDescifrar").value=mensajeDescifrado;
-    console.log(numeroDescifrar);
-    console.log(mensajeDescifrar);
-    console.log(mensajeDescifrado);
+    document.getElementById("mensajeDescifrar").value = mensajeDescifrado;
+    // console.log(numeroDescifrar);
+    // console.log(mensajeDescifrar);
+    // console.log(mensajeDescifrado);
 }
 descifrar.addEventListener("click", botonDescifrar);
 
-//copiar texto
-// const  copiar=() {
-//     let $temp = $("<input>")
-//     $("body").append($temp);
-//     $temp.val($(elemento).text()).select();
-//     document.execCommand("copy");
-//     $temp.remove();
-//   }
-
 //volver a 1era página
-const Volver= () => {
+const Volver = () => {
     document.querySelector(".primeraPagina").style.display = "block";
     document.querySelector(".segundaPagina").style.display = "none";
 }
 botonVolver.addEventListener("click", Volver);
 
+//console.log(cipher);
 
-
-
-
-
-console.log(cipher);
-
- 
